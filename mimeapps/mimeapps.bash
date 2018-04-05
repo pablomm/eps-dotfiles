30af7bbb59ddcb396350b0acf8eeb53dad86b097
pablomm <pablo.marcosm@estudiante.uam.es>
2018-04-05 19:03:35 +0200

    Modificado para que use variable de configuration

--- mimeapps/mimeapps.bash
+++ mimeapps/mimeapps.bash
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
+cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-#CONFIG="mimeapps.custom1.list"
-CONFIG="mimeapps.custom2.list"
+CONFIG=$mimeappsFile
 MIMEAPPS=$(echo -n "$HOME/.local/share/applications/mimeapps.list")
 BACK_UP="../backup/mimeapps.list.backup"
 BACK_UP_FOLDER="../backup"
 
 
-cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 # Backup
 if [ -e $MIMEAPPS ]; then
