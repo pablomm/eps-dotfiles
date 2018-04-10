10c13f764dc6811fdfa1e8f0cc84cfa60f8853ef
Pablo Marcos Manchón <pablomm@Pablos-MacBook-Air.local>
2018-04-10 19:31:58 +0200

    Dotfiles.bash acabado

---
+++ dotfiles/dotfiles.bash
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+#
+# File: dotfiles.bash
+# Date: 10/4/2018
+#
+# This file is part of eps-dotfiles.
+#
+#     Foobar is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     Foobar is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with eps-dotfiles.  If not, see <http://www.gnu.org/licenses/>.
+
+BACK_UP_FOLDER="./backup"
+EXCLUDE="dofiles.bash README.md"
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# Funcion para restaurar archivos
+function restore {
+
+	# Borramos archivos añadidos
+	for file in $(ls); do
+		if ! echo $EXCLUDE | grep -w $file > /dev/null; then
+			rm -f "$HOME/.$file"
+		fi
+	done
+
+	# Restauramos version antigua
+	for backup in $(ls -d $BACK_UP_FOLDER/*.backup); do
+
+		filename="$(basename $backup)"
+		filename="${filename%.*} "
+
+		cp "$backup" "$HOME/.$filename"
+	done
+
+
+}
+
+# Creamos carpeta backups si no existe
+if ! [ -e $BACK_UP_FOLDER ]; then
+	mkdir $BACK_UP_FOLDER
+fi
+
+# Caso restaurar configuracion anterior
+if [[ "$*" == "-r" ]] ; then
+	restore
+	exit 0
+fi
+
+for file in $(ls); do
+
+	# Iteramos
+	if ! echo $EXCLUDE | grep -w $file > /dev/null; then
+
+		# Si existe el fichero guardamos la copia en backups
+		if [ -e "$HOME/.$file" ]; then
+			cp "$HOME/.$file" "$BACK_UP_FOLDER/$file.backup"
+		fi
+
+		# Copiamos el archivo a la carpeta personal
+		cp "$file" "$HOME/.$file"
+
+	fi
+
+done
